20045b55a2ebb99af89f8bb73848ecc92ee3b65e
DimasKama <mynameisiphone10@gmail.com>
2025-08-30 18:42:36 +0500

    - Fix NeoForge incompatibility with 1.21.7 & 1.21.8

--- neoforge/src/main/java/ru/dimaskama/voicemessages/neoforge/VoiceMessagesNeoForge.java
+++ neoforge/src/main/java/ru/dimaskama/voicemessages/neoforge/VoiceMessagesNeoForge.java
@@ -57,7 +57,7 @@ public final class VoiceMessagesNeoForge {
 
             @Override
             public void sendToServer(CustomPacketPayload payload) {
-                PacketDistributor.sendToServer(payload);
+                net.neoforged.neoforge.client.network.ClientPacketDistributor.sendToServer(payload);
             }
 
             @Override
